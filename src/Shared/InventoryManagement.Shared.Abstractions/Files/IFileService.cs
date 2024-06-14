83ca5ec0447c1976ed615ceab32fe6d1f8079f94
rezafauzi123 <reza.fauzi@radyalabs.id>
2024-06-14 16:57:06 +0700

    init

---
+++ src/Shared/InventoryManagement.Shared.Abstractions/Files/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManagement.Shared.Abstractions.Files;
+
+public interface IFileService
+{
+    Task<FileResponse> UploadAsync(FileRequest request, CancellationToken cancellationToken);
+
+    Task<FileDownloadResponse?> DownloadAsync(string fileName, CancellationToken cancellationToken);
+}
